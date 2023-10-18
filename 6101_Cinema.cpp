5ce6e0be453f070ca97616850b5ff904f0af7f0d
ResArt2005 <resart2005@gmail.com>
2023-10-18 20:22:27 +0300

    Изменения, внесённые Артёмом
    Итак, отчёт по изменениям: 1) убрал лишний комментарии, которые были созданы компилятором 2) добавлены 3 функции для оплаты и 1 перечисление для выбор способа оплаты 3) Добавлен файл bestinput с одноимённым классом, который содержит в себе ввод данных с проверкой на мусор 4) в основном файле были добавлены библиотеки, которые будем использовать в будущем (сделал шаблон) 5) Сменил кодировку консоли, чтобы можно было комфортно работать с русским языком, а так же функция для рандомизации

--- 6101_Cinema.cpp
+++ 6101_Cinema.cpp
@@ -1,17 +1,45 @@
-﻿// 6101_Cinema.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿//Основаная библиотека
 #include <iostream>
+//Библиотека для работы со строками
+#include <string>
+//Библиотека для работы с файлами
+#include <fstream>
+//Библиотека для работы с вектором
+#include <vector>
+//Библиотека для смены кодировки консоли
+#include <Windows.h>
+//Библиотека для подключения алгоритмов сортировки или рандомизации чисел
+#include <algorithm>
+//Библиотека Артёма, где присутсвует класс, со встроенное проверкой вводных данных
+#include "bestinput.cpp"
 using namespace std;
 
 void show_cinema(bool**,int,int);
 bool buy_ticket(bool**, int, int); //принимает зал(массив) номер ряда номер места
 bool** create_hall(int,int);
 
+/*От Артёма: функции для имитации оплаты и покупки*/
+//Перечисление различных способов оплаты
+enum Operation {
+	Cash = 0,
+	Card = 1,
+	QRcode = 2
+};
+//Выбор метода оплаты
+void ChoosePaymentMethod();
+//Фуекция, где взависимости выбранного способа оплаты, будет происходить конкретная оплата
+bool Oplata(Operation);
+//Генерация QR-кода
+string QRgeneration();
+
 int main()
 {
+	srand(time(NULL));
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	bool** cinema1 = create_hall(3, 10);
 	show_cinema(cinema1, 3, 10);
+	ChoosePaymentMethod();
 }
 
 void show_cinema(bool** arr, int n, int m) {
@@ -45,13 +73,51 @@ bool buy_ticket(bool** arr, int n , int m) {
 	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+string QRgeneration() {
+	const int lengthOfCode = 20;
+	string QR = "";
+	for (int i = 0; i < lengthOfCode; i++)
+	{
+		QR += (char)(48 + rand() % 9);
+	}
+	return QR;
+}
+bool Oplata(Operation op) {
+	if (op == Cash) {
+		cout << "Производится полата...";
+		Sleep(1000);
+		cout << "\nОплата наличкой прошла успешно!";
+		return true;
+	}
+	else if (op == Card) {
+		cout << "Производится полата...";
+		Sleep(1000);
+		cout << "\nОплата по карте прошла успешно!";
+		return true;
+	}
+	else if (op == QRcode) {
+		cout << "Производится генерация вашего персонального QRcode...";
+		Sleep(1000);
+		cout << "\nВаш персональный QRcode: " << QRgeneration();
+		return true;
+	}
+}
+void ChoosePaymentMethod() {
+	BestInput input;
+	bool flag;
+	do
+	{
+		do
+		{
+			flag = false;
+			string temp;
+			cout << "\nВыбирете способ оплаты(qrcode/карта/наличные): ";
+			temp = input.InputString();
+			if (temp == "qrcode") { Oplata(QRcode); }
+			else if (temp == "карта") { Oplata(Card); }
+			else if (temp == "наличные") { Oplata(Cash); }
+			else { cout << "Вы ввели способ оплаты, которого у нас нет! Повторите запрос"; flag = true; }
+		} while (flag);
+		cout << "\nВы согласны с выбором? Если нет, мы оформим возврат(да/нет): ";
+	} while (input.YesOrNo() == false);
+}
---
+++ bestinput.cpp
@@ -0,0 +1,176 @@
+//Основаная библиотека
+#include <iostream>
+//Библиотека для работы со строками
+#include <string>
+using namespace std;
+/*Артём: для удобста я решил сделать класс,
+ который будет использоваться для валидации мусорных данных
+*/
+class BestInput {
+	string EraseNulls(string num) {
+		int poz1 = -1;
+		//Чтобы не стереть ноль, мы его запомним на позиции
+		int pozRem = 0;
+		//Замена нуля на посторонний символ
+		do
+		{
+			poz1 = num.find(",", poz1 + 1);
+			if (poz1 != num.npos)
+			{
+				if (num[poz1 - 1] == '0')
+				{
+					pozRem = poz1 - 1;
+					num[pozRem] = '~';
+				}
+			}
+		} while (poz1 != num.npos);//num.npos = -1
+		int poz = num.length();
+		do
+		{
+			//Ищет вхождение до точки или запятой 0
+			poz = num.find_first_of("0,.", poz - 1);
+			//Если позиция найдена
+			if (poz != num.npos)
+			{
+				//Стираем элемент строки на указаной позиции
+				num.erase(poz, 1);
+			}
+		} while (poz != num.npos);//пока не вернёт npos (значит, что символ в строке не найден)
+		//Возвращаем ноль на позицию, если он до этого был
+		if (num[pozRem] == '~')
+		{
+			num[pozRem] = '0';
+		}
+		return num;
+	}
+	string ReplaceDotToComma(string str) {
+		//Инициализируем позицию
+		int poz = -1;
+		do
+		{
+			//Ищет вхождение знака точки
+			poz = str.find('.', poz + 1);
+			//Если позиция найдена
+			if (poz != str.npos)
+			{
+				//Стираем точку на указаной позиции
+				str.erase(poz, 1);
+				//Вставляем запятую на указаной позиции
+				str.insert(poz, 1, ',');
+			}
+			//str.npos - константа, возвращающая -1
+		} while (poz != str.npos);//пока не вернёт npos (значит, что символ в строке не найден)
+		return str;
+	}
+	string ReplaceCommaToDot(string str) {
+		//Инициализируем позицию
+		int poz = -1;
+		do
+		{
+			//Ищет вхождение знака точки
+			poz = str.find(',', poz + 1);
+			//Если позиция найдена
+			if (poz != str.npos)
+			{
+				//Стираем точку на указаной позиции
+				str.erase(poz, 1);
+				//Вставляем запятую на указаной позиции
+				str.insert(poz, 1, '.');
+			}
+			//str.npos - константа, возвращающая -1
+		} while (poz != str.npos);//пока не вернёт npos (значит, что символ в строке не найден)
+		return str;
+	}
+public:
+	double InputDouble() {
+		string str;
+		bool flag = true;
+		double num;
+		do {
+			//Если функция stod поймает исключение, то вместо прерывания программы, пользователю скажут, что данные некорректны
+			//И попросят ввести снова
+			try {
+				//Ввод числа как строки
+				getline(cin, str);
+				//Замена точек на запятые или наооборот (зависит от setlocale()) для дальнейсшей конвертации в double
+				//str = ReplaceDotToComma(str); //Если местоположение было иземенено setlocale()
+				str = ReplaceCommaToDot(str);	//Если местоположение не было иземенено setlocale()
+				//Конвертация string в double
+				num = stod(str);
+				flag = false;
+				//stod отбрасывает символы после чисел, вместо выкидывания исключения, поэтому делаем дополнительную проверку
+				if ((EraseNulls(to_string(num)).compare(str) != 0)) {
+					cout << "Incorrect input, enter a satisfactory value: ";
+					flag = true;
+				}
+			}
+			catch (...)
+			{
+				cout << "Incorrect input, enter a satisfactory value: ";
+			}
+		} while (flag);
+		return num;
+	}
+	int InputInt() {
+		string str;
+		bool flag = true;
+		int number;
+		do {
+			try {
+				getline(cin, str);
+				number = stoi(str.c_str());
+				flag = false;
+				//Функция stoi игнорирует точки и сиволы после числовых значений в строке
+				//Поэтому числовое и строковое знаечения могут отличаться по длине
+				if (to_string(number).compare(str) != 0) {
+					cout << "Incorrect input, enter an integer: ";
+					flag = true;
+				}
+			}
+			catch (...)
+			{
+				cout << "Incorrect input, enter a satisfactory value: ";
+			}
+		} while (flag);
+		return number;
+	}
+	string InputString() {
+		string str;
+		getline(cin, str);
+		return str;
+	}
+	char InputChar() {
+		char ch;
+		bool flag = true;
+		do
+		{
+			string str;
+			getline(cin, str);
+			if (str.length() == 1) {
+				flag = false;
+				ch = str[0];
+			}
+			else {
+				cout << "Enter character: ";
+			}
+		} while (flag);
+
+		return ch;
+	}
+	bool YesOrNo() {
+		bool flag = true;
+		string tryagain;
+		//Повтор игры
+		do
+		{
+			getline(cin, tryagain);
+			if (tryagain == "да" || tryagain == "нет" || tryagain == "yes" || tryagain == "no") {
+				flag = false;
+			}
+			else {
+				cout << "\nНеизвестная команда! Введите (да/нет): ";
+			}
+		} while (flag);
+		return (tryagain == "да" || tryagain == "yes") ? true : false;
+	}
+};
